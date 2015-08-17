6ba066791f0eb48069739dda975a7488c5b07bfa
JCassidy7 <jessicaacassidy@gmail.com>
2015-08-17 10:26:54 -0400

    Create SortInterface.java

---
+++ SortInterface.java
@@ -0,0 +1,16 @@
+package sortmain;
+
+/**
+ *
+ * @author Jessica Cassidy
+ */
+public interface SortInterface {
+    
+    void iterativeSort (int [] list) throws Exception;
+    void recursiveSort (int [] list) throws Exception;
+    
+    public int getCount();
+    
+    public long getTime();
+    
+}
