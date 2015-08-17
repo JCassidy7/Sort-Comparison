602c374b1b7645a492cedbf9efbdd03ffb6638db
JCassidy7 <jessicaacassidy@gmail.com>
2015-08-17 10:27:47 -0400

    Create UnsortedException.java

---
+++ UnsortedException.java
@@ -0,0 +1,17 @@
+package sortmain;
+
+/**
+ *
+ * @author Jessica Cassidy
+ */
+public class UnsortedException extends Exception{
+    
+    public UnsortedException(){
+     
+    }
+    
+    public UnsortedException (String message){
+        super(message);
+    }
+    
+}
