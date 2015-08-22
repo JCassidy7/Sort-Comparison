2eecc5627b0eb94d92a74ee9c244df7063c92db2
JCassidy7 <jessicaacassidy@gmail.com>
2015-08-22 11:14:41 -0400

    Update SortMain.java

--- SortMain.java
+++ SortMain.java
@@ -10,7 +10,7 @@ public class SortMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception{
-        int[] sizes = {10, 50, 100, 200, 1000, 5000, 10000, 20000, 50000, 100000} ; 
+        int[] sizes = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000} ; 
         BenchmarkSorts bSorts = new BenchmarkSorts(sizes);
       }
     
